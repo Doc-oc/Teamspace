064587a519b6c68dea988d223f2f212bdbc19dd1
Dylan <dylanoconnor32@gmail.com>
2022-01-28 15:15:08 +0000

    Creating SignUp Screen

--- teamspace/src/firebase.js
+++ teamspace/src/firebase.js
@@ -1,20 +1,15 @@
-import app from 'firebase/app';
+import firebase from "firebase/app"
+import "firebase/auth";
 
-const firebaseConfig = {
+const app = firebase.initializeApp({
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_DATABASE_URL,
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    //appId: "1:166500978414:web:297823319288eb0ce9edff",
+    appId: "1:166500978414:web:297823319288eb0ce9edff",
     //measurementId: "G-BPJFH3RB84"
-};
+});
 
-class Firebase {
-    constructor() {
-        app.initializeApp(firebaseConfig);
-    }
-}
-
-export default Firebase;
+export default app;
