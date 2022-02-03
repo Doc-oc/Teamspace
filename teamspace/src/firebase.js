f785cc90279ca7efbdf445ad98822082f5364386
Dylan <dylanoconnor32@gmail.com>
2022-02-03 14:28:38 +0000

    Signup working

--- teamspace/src/firebase.js
+++ teamspace/src/firebase.js
@@ -1,5 +1,5 @@
-import firebase from "firebase/app"
-import "firebase/auth";
+import firebase from "firebase/compat/app"
+import "firebase/compat/auth";
 
 const app = firebase.initializeApp({
     apiKey: process.env.REACT_APP_API_KEY,
@@ -8,9 +8,9 @@ const app = firebase.initializeApp({
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: "1:166500978414:web:297823319288eb0ce9edff"
+    appId: "1:166500978414:web:297823319288eb0ce9edff",
     //measurementId: "G-BPJFH3RB84"
 });
 
 export const auth = app.auth()
-export default app; 
+export default app
