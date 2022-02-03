78d1fcd2d133c5e3c44f991ae757e0318ef8cb3b
Dylan <dylanoconnor32@gmail.com>
2022-02-03 14:51:19 +0000

    Routes between pages

--- teamspace/src/components/App.js
+++ teamspace/src/components/App.js
@@ -2,16 +2,25 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import { AuthProvider } from "../context/AuthContext";
 import Signup from "./Signup";
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from "./Home"
+import Login from "./Login"
 
 function App() {
   return (
-    <AuthProvider>
       <Container className="d-flex align-items-center justify-content-center" style={{minHeight: "100vh"}}>
         <div className="w-100" style={{maxWidth: "400px"}}>
-          <Signup />
+          <Router> 
+            <AuthProvider>
+              <Routes>
+                <Route exact path="/" element={<Home/>} />
+                <Route path="/signup" element={<Signup/>} />
+                <Route path="/login" element={<Login/>} />
+              </Routes>
+            </AuthProvider>
+          </Router>
         </div>
       </Container>
-    </AuthProvider>
   )
 }
 
--- teamspace/src/components/Login.js
+++ teamspace/src/components/Login.js
@@ -1,23 +1,18 @@
 import React , {useRef, useState} from 'react'
 import {Button, Card, Form, Alert} from 'react-bootstrap';
 import {useAuth} from '../context/AuthContext'
-import { Link } from "react-router-dom"
+import {Link} from "react-router-dom"
 
-export default function Signup(){
+export default function Login(){
 
     const emailRef = useRef()
     const passwordRef = useRef()
-    const passwordConfirmRef = useRef()
     const { signup, currentUser } = useAuth()
     const [error, setError] = useState('')
     const [loading, setLoading] = useState(false)
 
     async function handleSubmit(e){
         e.preventDefault()
-
-        if(passwordRef.current.value !== passwordConfirmRef.current.value){
-            return setError("Passwords do not match")
-        }
 
         try {
             setError("")
@@ -35,7 +30,7 @@ export default function Signup(){
         <>
             <Card>
                 <Card.Body>
-                    <h2 className="text-center mb-4">SignUp</h2>
+                    <h2 className="text-center mb-4">Login</h2>
                     {currentUser.email}
                     {error && <Alert variant="danger">{error}</Alert>}
                     <Form onSubmit={handleSubmit}>
@@ -47,16 +42,12 @@ export default function Signup(){
                             <Form.Label>Password</Form.Label>
                             <Form.Control type="password" ref={passwordRef} required />
                         </Form.Group>
-                        <Form.Group id="passwordConfirm">
-                            <Form.Label>Password Confirmation</Form.Label>
-                            <Form.Control type="password" ref={passwordConfirmRef} required />
-                        </Form.Group>
-                        <Button disabled={loading} className="w-100" type="submit">Sign Up</Button>
+                        <Button disabled={loading} className="w-100" type="submit">Login</Button>
                     </Form>
                 </Card.Body>
             </Card>
             <div className="w-100 text-center mt-2">
-            Already Have an Account? <Link to="/login">Login</Link>
+            Need an Account? <Link to="/signup">Sign up</Link>
             </div>
         </>
     )
