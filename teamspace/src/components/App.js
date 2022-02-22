991a58013069d9e97f735ddf88db252ab18f3127
Dylan <dylanoconnor32@gmail.com>
2022-02-22 23:50:42 +0000

    Adding server + Home Page Columns

--- teamspace/src/components/App.js
+++ teamspace/src/components/App.js
@@ -6,6 +6,24 @@ import { BrowserRouter , Outlet, Navigate, Router, Routes, Route} from 'react-ro
 import Home from "./Home"
 import Login from "./Login"
 
+/*state = { data: null };
+
+function componentDidMount() {
+  this.callBackendAPI()
+    .then(res => this.setState({ data: res.express }))
+    .catch(err => console.log(err));
+}
+  // fetching the GET route from the Express server which matches the GET route from server.js
+callBackendAPI = async () => {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message) 
+    }
+    return body;
+  };*/
+
 //Checking if user is logged in 
 function PrivateRoute({ children }) {
   const auth = useAuth();
--- teamspace/src/components/Home.js
+++ teamspace/src/components/Home.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react"
-import { Card, Button, Alert } from "react-bootstrap"
+import {Button, Card, Form, Alert, Container} from 'react-bootstrap';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 import { useAuth } from "../context/AuthContext"
 import { useNavigate } from "react-router-dom"
 
@@ -23,19 +25,38 @@ export default function Home() {
   }
 
   return (
-    <>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Profile</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          <strong>Email:</strong> {currentUser?.email}
-        </Card.Body>
-      </Card>
-      <div className="w-100 text-center mt-2">
-        <Button variant="link" onClick={handleLogout}>
-          Log Out
-        </Button>
-      </div>
-    </>
+
+    <Container fluid className="" style={{minHeight: "100vh"}}>
+        <Row>  
+          <Col className="col-sm-3">
+            <Card className="shadow">
+              <Card.Body>
+                <h2 className="text-center mb-4">Profile</h2>
+                {error && <Alert variant="danger">{error}</Alert>}
+                <strong>Email:</strong> {currentUser?.email}
+              </Card.Body>
+            </Card >
+            <div className="w-100 text-center mt-2">
+              <Button variant="link" onClick={handleLogout}>
+                Log Out
+              </Button>
+            </div>
+            </Col>
+            <Col className="col-sm-6">
+            <Card className="shadow">
+              <Card.Body>
+                <p>Main Section</p>
+              </Card.Body>
+            </Card>
+            </Col>
+            <Col className="col-sm-3">
+            <Card className="shadow">
+              <Card.Body>
+                <p>Side Bar</p>
+              </Card.Body>
+            </Card>
+            </Col>
+          </Row>
+      </Container>
   )
 }
