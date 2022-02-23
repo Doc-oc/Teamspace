1faeb62fa49fcd1e1f349ac4414e3030e3d28c3c
Dylan <dylanoconnor32@gmail.com>
2022-02-23 16:01:12 +0000

    Adding Navigation Bar

--- teamspace/src/components/Home.js
+++ teamspace/src/components/Home.js
@@ -1,9 +1,13 @@
 import React, { useState } from "react"
-import {Button, Card, Form, Alert, Container} from 'react-bootstrap';
+import {Button, Card, Form, Alert, Container, Navbar, Nav} from 'react-bootstrap';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import { useAuth } from "../context/AuthContext"
 import { useNavigate } from "react-router-dom"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSlidersH, faClipboard, faUser, faSignOutAlt } from '@fortawesome/fontawesome-free-solid'
+import '../home.css'
+import pp from '../img/blank.webp'; // Tell webpack this JS file uses this image
 
 export default function Home() {
   const [error, setError] = useState("")
@@ -28,24 +32,47 @@ export default function Home() {
 
     <Container fluid className="mt-5" style={{minHeight: "100vh"}}>
         <Row>  
-          <Col className="col-sm-2">
-            <Card className="shadow">
+          <Col className="col-sm-3">
+            <Card className="shadow text-center">
               <Card.Body>
+              <Container>
                 <h2 className="text-center mb-4">Profile</h2>
+                <br></br>
+                <img src="#" className="img-responsive w-50 roundedCircle"></img>
+                <br></br>
                 {error && <Alert variant="danger">{error}</Alert>}
-                <strong>Email:</strong> {currentUser?.email}
+                <strong className="text-center">User:</strong> {currentUser?.email}
+                <br></br>
+                <Nav className="col-md-12 d-none d-md-block mt-5 mb-5 sidebar text-center navbar-custom" activeKey="/home" conSelect={selectedKey => alert(`selected ${selectedKey}`)}>
+                <div className="sidebar-sticky"></div>
+                  <Nav.Item>
+                  <Nav.Link eventKey="Profile"><FontAwesomeIcon icon={faUser}/> Profile</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link href="/"><FontAwesomeIcon icon={faClipboard}/>  Boards</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link eventKey="Settings"><FontAwesomeIcon icon={faSlidersH}/> Settings</Nav.Link>
+                  </Nav.Item>
+                    <Nav.Item>
+                  </Nav.Item>
+                </Nav>
+              </Container>
               </Card.Body>
-            </Card >
-            <div className="w-100 text-center mt-2">
-              <Button variant="link" onClick={handleLogout}>
-                Log Out
-              </Button>
-            </div>
-            </Col>
-            <Col className="col-sm-7">
+              <div className="w-100 text-center mt-2">
+                <Button className="logout btn-primary" onClick={handleLogout}>
+                  <FontAwesomeIcon icon={faSignOutAlt}/> Log Out
+                </Button>
+              </div>
+          </Card> 
+        </Col>
+            <Col className="col-sm-6">
             <Card className="shadow">
               <Card.Body>
                 <p>Main Section</p>
+                <form action="../post" method="post" className="form">
+                  <button type="submit">Connected to Server</button>
+                </form>
               </Card.Body>
             </Card>
             </Col>
--- teamspace/src/server/server.js
+++ teamspace/src/server/server.js
@@ -1,11 +1,11 @@
-const express = require('express'); //Line 1
-const app = express(); //Line 2
-const port = process.env.PORT || 5000; //Line 3
-
-// This displays message that the server running and listening to specified port
-app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
-
-// create a GET route
-app.get('/express_backend', (req, res) => { //Line 9
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
-}); //
+const express = require("express");
+const app = express();
+  
+app.post("/post", (req, res) => {
+  console.log("Connected to React");
+  res.redirect("/");
+});
+  
+const PORT = process.env.PORT || 8080;
+  
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
