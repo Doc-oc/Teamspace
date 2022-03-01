e3da0639fa0eb2983fde350af649fc0315aeae1e
Dylan <dylanoconnor32@gmail.com>
2022-03-01 22:53:44 +0000

    Fixing Home Front-end

--- teamspace/src/components/Home.js
+++ teamspace/src/components/Home.js
@@ -7,7 +7,6 @@ import { useNavigate } from "react-router-dom"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSlidersH, faClipboard, faUser, faSignOutAlt } from '@fortawesome/fontawesome-free-solid'
 import '../home.css'
-import pp from '../img/blank.webp'; // Tell webpack this JS file uses this image
 
 export default function Home() {
   const [error, setError] = useState("")
@@ -30,18 +29,18 @@ export default function Home() {
   }
   return (
 
-    <Container fluid className="mt-5" style={{minHeight: "100vh"}}>
+    <Container fluid className="mt-3" style={{minHeight: "100vh"}}>
         <Row>  
-          <Col className="col-sm-3">
-            <Card className="shadow text-center">
+          <Col className="col-sm-2">
+            <Card className="shadow text-center" style={{minHeight: "600px", borderRadius: 15}}>
               <Card.Body>
               <Container>
-                <h2 className="text-center mb-4">Profile</h2>
+              <h6 className="mb-5 " style={{color: "#4176FF"}}>Teamspace</h6>
                 <br></br>
-                <img src="#" className="img-responsive w-50 roundedCircle"></img>
+                <img src="#" className="img-responsive w-50 mt-5 roundedCircle"></img>
                 <br></br>
                 {error && <Alert variant="danger">{error}</Alert>}
-                <strong className="text-center">User:</strong> {currentUser?.email}
+                {currentUser?.email}
                 <br></br>
                 <Nav className="col-md-12 d-none d-md-block mt-5 mb-5 sidebar text-center navbar-custom" activeKey="/home" conSelect={selectedKey => alert(`selected ${selectedKey}`)}>
                 <div className="sidebar-sticky"></div>
@@ -60,24 +59,25 @@ export default function Home() {
               </Container>
               </Card.Body>
               <div className="w-100 text-center mt-2">
-                <Button className="logout btn-primary" onClick={handleLogout}>
+                <Button className="logout mb-2" onClick={handleLogout}>
                   <FontAwesomeIcon icon={faSignOutAlt}/> Log Out
                 </Button>
               </div>
           </Card> 
         </Col>
-            <Col className="col-sm-6">
-            <Card className="shadow">
+            <Col className="col-sm-7">
+            <Card className="shadow" style={{minHeight: "600px", borderRadius: 15}}>
               <Card.Body>
                 <p>Main Section</p>
-                <form action="../post" method="post" className="form">
-                  <button type="submit">Connected to Server</button>
-                </form>
+                
               </Card.Body>
             </Card>
+              <form action="../post" method="post" className="form">
+                <button className="btn-primary rounded shadow" style={{backgroundColor: "#4176FF"}} type="submit">Connect to Server</button>
+              </form>
             </Col>
             <Col className="col-sm-3">
-            <Card className="shadow">
+            <Card className="shadow" style={{minHeight: "600px", borderRadius: 15}}>
               <Card.Body>
                 <p>Side Bar</p>
               </Card.Body>
